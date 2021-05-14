68fc7326f114e0e4fa19909165e6632bb5ace5ed
HouKunLin <houkunlin@aliyun.com>
2021-05-14 21:18:30 +0800

    test: 修改 Tests 的 Demo 配置信息

--- src/test/java/test/application/server/local/DicConfiguration.java
+++ src/test/java/test/application/server/local/DicConfiguration.java
@@ -1,8 +1,10 @@
 package test.application.server.local;
 
-import com.houkunlin.system.dic.starter.store.DicStore;
+import com.houkunlin.system.dic.starter.bean.DicTypeVo;
 import com.houkunlin.system.dic.starter.store.LocalDicStore;
 import com.houkunlin.system.dic.starter.store.RemoteDic;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,8 +13,27 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 public class DicConfiguration {
+    private static final Logger logger = LoggerFactory.getLogger(DicConfiguration.class);
+
     @Bean
-    public DicStore dicStore(final RemoteDic remoteDic) {
+    public LocalDicStore localDicStore(final RemoteDic remoteDic) {
+        logger.debug("使用自定义的 LocalDicStore 存储数据字典信息");
         return new LocalDicStore(remoteDic);
+    }
+
+    @Bean
+    public RemoteDic remoteDic() {
+        logger.debug("提供一个空的 RemoteDic 对象");
+        return new RemoteDic() {
+            @Override
+            public DicTypeVo getDicType(final String type) {
+                return null;
+            }
+
+            @Override
+            public String getDicValueTitle(final String type, final String value) {
+                return null;
+            }
+        };
     }
 }
--- src/test/java/test/application/server/redis/DicConfiguration.java
+++ src/test/java/test/application/server/redis/DicConfiguration.java
@@ -1,8 +1,10 @@
 package test.application.server.redis;
 
-import com.houkunlin.system.dic.starter.store.DicStore;
+import com.houkunlin.system.dic.starter.bean.DicTypeVo;
 import com.houkunlin.system.dic.starter.store.RedisDicStore;
 import com.houkunlin.system.dic.starter.store.RemoteDic;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -12,8 +14,27 @@ import org.springframework.data.redis.core.RedisTemplate;
  */
 @Configuration
 public class DicConfiguration {
+    private static final Logger logger = LoggerFactory.getLogger(DicConfiguration.class);
+
     @Bean
-    public DicStore dicStore(final RedisTemplate<Object, Object> redisTemplate, final RemoteDic remoteDic) {
+    public RedisDicStore redisDicStore(final RedisTemplate<Object, Object> redisTemplate, final RemoteDic remoteDic) {
+        logger.debug("使用自定义的 RedisDicStore 存储数据字典信息");
         return new RedisDicStore(redisTemplate, remoteDic);
+    }
+
+    @Bean
+    public RemoteDic remoteDic() {
+        logger.debug("提供一个空的 RemoteDic 对象");
+        return new RemoteDic() {
+            @Override
+            public DicTypeVo getDicType(final String type) {
+                return null;
+            }
+
+            @Override
+            public String getDicValueTitle(final String type, final String value) {
+                return null;
+            }
+        };
     }
 }
