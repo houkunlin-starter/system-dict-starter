1d7dfd3ef9a6473edc42ed7e99081616c1e0d7f1
HouKunLin <houkunlin@aliyun.com>
2021-08-29 11:15:49 +0800

    fix: 修复 SpringBoot 2.5.3 下打包错误问题
    提示 spring-configuration-metadata.json 文件重复无法打包，因为 SpringBoot 插件会自动生成 spring-configuration-metadata.json 的配置文件代码提示功能

--- src/main/java/com/houkunlin/system/dict/starter/DictProperties.java
+++ src/main/java/com/houkunlin/system/dict/starter/DictProperties.java
@@ -2,6 +2,7 @@ package com.houkunlin.system.dict.starter;
 
 import com.houkunlin.system.dict.starter.notice.RefreshDictEvent;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.ToString;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +14,7 @@ import org.springframework.context.annotation.Configuration;
  */
 @Data
 @ToString
+@EqualsAndHashCode
 @Configuration
 @ConfigurationProperties("system.dict")
 public class DictProperties {
