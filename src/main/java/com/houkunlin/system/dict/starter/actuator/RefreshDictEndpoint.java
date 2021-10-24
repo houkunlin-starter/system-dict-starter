7d87c70b31296668648df48fe4c672018bae782b
HouKunLin <houkunlin@aliyun.com>
2021-10-24 22:59:14 +0800

    feat: 增加一个刷新字典的端点

---
+++ src/main/java/com/houkunlin/system/dict/starter/actuator/RefreshDictEndpoint.java
@@ -0,0 +1,84 @@
+package com.houkunlin.system.dict.starter.actuator;
+
+import com.houkunlin.system.dict.starter.notice.RefreshDictEvent;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
+import org.springframework.boot.actuate.endpoint.annotation.Selector;
+import org.springframework.boot.actuate.endpoint.annotation.WriteOperation;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * 字典刷新端点
+ *
+ * @author HouKunLin
+ * @since 1.4.3.4
+ */
+@Component
+@Endpoint(id = "dictRefresh")
+@AllArgsConstructor
+public class RefreshDictEndpoint implements InitializingBean {
+    private final ApplicationEventPublisher applicationEventPublisher;
+    private final Map<String, Object> map = new HashMap<>();
+    private final Map<String, Object> mapErr = new HashMap<>();
+
+    /**
+     * 默认端点接口信息
+     *
+     * @return 返回系统一些class对象名称
+     */
+    @WriteOperation
+    public Object index() {
+        applicationEventPublisher.publishEvent(new RefreshDictEvent("Endpoint/dictRefresh"));
+        return map;
+    }
+
+    @WriteOperation
+    public Object indexMsg(@Selector String msg) {
+        if (!StringUtils.hasText(msg)) {
+            return mapErr;
+        }
+        applicationEventPublisher.publishEvent(new RefreshDictEvent(msg));
+        return map;
+    }
+
+    @WriteOperation
+    public Object indexMsgNotify(@Selector String msg, @Selector boolean notify) {
+        if (!StringUtils.hasText(msg)) {
+            return mapErr;
+        }
+        applicationEventPublisher.publishEvent(new RefreshDictEvent(msg, notify));
+        return map;
+    }
+
+    @WriteOperation
+    public Object indexMsgNotifyBrother(@Selector String msg, @Selector boolean notify, @Selector boolean notifyBrother) {
+        if (!StringUtils.hasText(msg)) {
+            return mapErr;
+        }
+        applicationEventPublisher.publishEvent(new RefreshDictEvent(msg, notify, notifyBrother));
+        return map;
+    }
+
+    @WriteOperation
+    public Object indexMsgNotifyBrotherClasses(@Selector String msg, @Selector boolean notify, @Selector boolean notifyBrother, @Selector Set<String> classes) {
+        if (!StringUtils.hasText(msg)) {
+            return mapErr;
+        }
+        applicationEventPublisher.publishEvent(new RefreshDictEvent(msg, notify, notifyBrother, classes));
+        return map;
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        map.put("result", "ok");
+        mapErr.put("result", "fail");
+        mapErr.put("msg", "msg must not blank");
+    }
+}
