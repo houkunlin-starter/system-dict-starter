5a0fc730bf3291a5fcbd3478b5aff214e81b5689
HouKunLin <houkunlin@aliyun.com>
2025-06-03 20:15:07 +0800

    feat: 移除类加载中Java8的支持代码

--- src/main/java/com/houkunlin/system/dict/starter/ClassUtil.java
+++ src/main/java/com/houkunlin/system/dict/starter/ClassUtil.java
@@ -5,28 +5,33 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.lang.reflect.Constructor;
 
+/**
+ * 自定义类工具
+ *
+ * @author HouKunLin
+ */
 @Slf4j
 public class ClassUtil {
+    /**
+     * 自定义的字节码类加载器
+     */
+    public static final BytecodeClassLoader CLASS_LOADER;
+
+    /**
+     * 私有构造方法
+     */
     private ClassUtil() {
     }
 
-    public static final BytecodeClassLoader CLASS_LOADER;
-    /**
-     * 运行环境版本是否小于等于 Java8
-     */
-    public static final boolean lessEqJava8;
-
     static {
-        String version = System.getProperty("java.version");
         CLASS_LOADER = new BytecodeClassLoader(Thread.currentThread().getContextClassLoader());
-        // 1.8.0_452 or 11.0.24
-        lessEqJava8 = version != null && version.startsWith("1.");
     }
 
     /**
      * 获取一个类的默认构造方法
      *
      * @param clazz 类对象
+     * @param <T> 类
      * @return 默认构造方法
      */
     @SuppressWarnings({"unchecked"})
@@ -40,17 +45,29 @@ public class ClassUtil {
         return null;
     }
 
+    /**
+     * 动态加载字节码类
+     *
+     * @param neighbor 新类的邻居类对象
+     * @param name     类名
+     * @param b        字节码
+     * @return 类对象
+     */
     public static Class<?> define(Class<?> neighbor, String name, byte[] b) {
-        Class<?> re = null;
-        if (lessEqJava8) {
-            re = ClassUtilJava8.define(neighbor, name, b);
-        }
+        Class<?> re = ClassUtilJava11.define(neighbor, name, b);
         if (re != null) {
             return re;
         }
         return CLASS_LOADER.define(name, b);
     }
 
+    /**
+     * 通过类名加载类对象
+     *
+     * @param className 类名
+     * @return 类对象
+     * @throws ClassNotFoundException 加载类失败异常
+     */
     public static Class<?> forName(String className) throws ClassNotFoundException {
         try {
             return Class.forName(className);
