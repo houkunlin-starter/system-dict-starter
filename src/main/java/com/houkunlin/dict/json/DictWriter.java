c8929d753cf0b3e302cee02f48b83e5dc7322dcf
HouKunLin <houkunlin@aliyun.com>
2026-01-18 22:42:29 +0800

    refactor: 删除 DictWriter 中不需要的代码，增加一些值类型处理

--- src/main/java/com/houkunlin/dict/json/DictWriter.java
+++ src/main/java/com/houkunlin/dict/json/DictWriter.java
@@ -1,7 +1,6 @@
 package com.houkunlin.dict.json;
 
 import com.houkunlin.dict.DictEnum;
-import com.houkunlin.dict.SystemDictAutoConfiguration;
 import com.houkunlin.dict.annotation.DictText;
 import tools.jackson.core.JacksonException;
 import tools.jackson.core.JsonGenerator;
@@ -51,8 +50,12 @@ public class DictWriter {
                 writeDictValue(gen, (Object[]) value, dictText, true);
             } else if (value instanceof Collection<?> v) {
                 writeDictValue(gen, v, dictText, true);
+            } else if (value instanceof Iterable<?> v) {
+                writeDictValue(gen, v, dictText, true);
             } else if (value instanceof DictEnum<?> v) {
                 writeDictValue(gen, v.getValue(), dictText, true);
+            } else if (value.getClass().isEnum()) {
+                gen.writeString(value.toString());
             } else if (value instanceof String v) {
                 gen.writeString(v);
             } else if (value instanceof BigDecimal v) {
@@ -69,10 +72,16 @@ public class DictWriter {
                 writeDictValue(gen, (Object[]) value, dictText, false);
             } else if (value instanceof Collection<?> v) {
                 writeDictValue(gen, v, dictText, false);
+            } else if (value instanceof Iterable<?> v) {
+                writeDictValue(gen, v, dictText, false);
             } else if (value instanceof DictEnum<?> v) {
                 gen.writeString(v.getValue().toString());
+            } else if (value.getClass().isEnum()) {
+                gen.writeString(value.toString());
             } else if (value instanceof String v) {
                 gen.writeString(v);
+            } else if (value instanceof BigDecimal v) {
+                gen.writeString(v.toPlainString());
             } else {
                 gen.writeString(value.toString());
             }
@@ -120,73 +129,21 @@ public class DictWriter {
     }
 
     /**
-     * 写入字典文本到JSON生成器
+     * 处理集合的字典值序列化
      * <p>
-     * 专门用于序列化字典的文本描述信息，对于DictEnum类型会获取其标题文本，
-     * 其他类型则转换为字符串格式
-     * </p>
-     *
-     * @param gen      JSON生成器
-     * @param value    要序列化的字典值
-     * @param dictText 字典文本配置信息，包含空值处理策略等配置
-     * @throws JacksonException JSON序列化异常
-     */
-    public void writeDictText(JsonGenerator gen, Object value, DictText dictText) throws JacksonException {
-        if (value == null) {
-            if (dictText.nullable().getValue(SystemDictAutoConfiguration::isTextValueDefaultNull)) {
-                gen.writeNull();
-            } else {
-                gen.writeString("");
-            }
-            return;
-        }
-        if (value.getClass().isArray()) {
-            writeDictText(gen, (Object[]) value, dictText);
-        } else if (value instanceof Collection<?> v) {
-            writeDictText(gen, v, dictText);
-        } else if (value instanceof DictEnum<?> v) {
-            gen.writeString(v.getTitle());
-        } else if (value instanceof String v) {
-            gen.writeString(v);
-        } else {
-            gen.writeString(value.toString());
-        }
-    }
-
-    /**
-     * 处理对象数组的字典文本序列化
-     * <p>
-     * 将对象数组序列化为JSON数组格式，递归处理数组中的每个元素的文本描述
-     * </p>
-     *
-     * @param gen      JSON生成器
-     * @param value    对象数组
-     * @param dictText 字典文本配置信息
-     * @throws JacksonException JSON序列化异常
-     */
-    private void writeDictText(JsonGenerator gen, Object[] value, DictText dictText) throws JacksonException {
-        gen.writeStartArray(value);
-        for (Object o : value) {
-            writeDictText(gen, o, dictText);
-        }
-        gen.writeEndArray();
-    }
-
-    /**
-     * 处理集合的字典文本序列化
-     * <p>
-     * 将集合序列化为JSON数组格式，递归处理集合中的每个元素的文本描述
+     * 将集合序列化为JSON数组格式，递归处理集合中的每个元素
      * </p>
      *
      * @param gen      JSON生成器
      * @param value    集合对象
      * @param dictText 字典文本配置信息
+     * @param useRawValueType 是否使用原始值模式
      * @throws JacksonException JSON序列化异常
      */
-    private void writeDictText(JsonGenerator gen, Collection<?> value, DictText dictText) throws JacksonException {
+    private void writeDictValue(JsonGenerator gen, Iterable<?> value, DictText dictText, boolean useRawValueType) throws JacksonException {
         gen.writeStartArray(value);
         for (Object o : value) {
-            writeDictText(gen, o, dictText);
+            writeDictValue(gen, o, dictText, useRawValueType);
         }
         gen.writeEndArray();
     }
