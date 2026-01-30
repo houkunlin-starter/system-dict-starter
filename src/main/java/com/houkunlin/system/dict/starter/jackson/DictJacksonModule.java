ec1b91a6582ed3fe3515ca99c6bf2752445a9578
HouKunLin <houkunlin@aliyun.com>
2026-01-31 00:18:49 +0800

    feat: update DictJacksonModule version

--- src/main/java/com/houkunlin/system/dict/starter/jackson/DictJacksonModule.java
+++ src/main/java/com/houkunlin/system/dict/starter/jackson/DictJacksonModule.java
@@ -12,7 +12,7 @@ import com.fasterxml.jackson.databind.module.SimpleModule;
  */
 public class DictJacksonModule extends SimpleModule {
     public DictJacksonModule() {
-        super("dictJacksonModule", new Version(1, 0, 0, null, "com.houkunlin.system.dict.starter.jackson", "dictJacksonModule"));
+        super("com.houkunlin.dict.jackson.DictJacksonModule", new Version(1, 0, 0, null, "com.houkunlin", "system-dict-starter"));
     }
 
     @Override
